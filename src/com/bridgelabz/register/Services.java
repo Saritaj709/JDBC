a9adb562aa49f2582c91d8619aabdbbbfa42468f
Saritaj709 <Saritaj709@gmail.com>
2018-07-02 18:52:56 +0530

    added

--- src/com/bridgelabz/register/Services.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.register;
-
-import java.sql.SQLException;
-
-public interface Services {
-public void login() throws SQLException;
-public void register() throws SQLException;
-}
